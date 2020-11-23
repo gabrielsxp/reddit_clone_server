af55f7f36e1baf148f81014cfd916011db981c6e
Gabriel Soares <gabrielsxp@hotmail.com>
2020-11-23 08:38:15 -0300

    installed express + graphql supporpt

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log('hello world')
+import { MikroORM } from "@mikro-orm/core";
+import mikroConfig from "./mikro-orm.config";
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
+import { PostResolver } from './resolvers/post'
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroConfig);
+  orm.getMigrator().up();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver],
+      validate: false
+    })
+  })
+
+  apolloServer.applyMiddleware({ app })
+
+  app.listen(4000, () => {
+    console.log('Server running on localhost:4000');
+  })
+};
+
+main().catch((err) => {
+  console.error(err)
+});
